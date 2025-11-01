89854f4bc6770d7c12991ef05a9c40c7e1d60e1d
Maildo Vitor Barros Junior <maildo.junior@oi.net.br>
2025-11-01 09:11:44 -0300

    Adding Env

--- Program.cs
+++ Program.cs
@@ -4,6 +4,9 @@ using TodoApi.Core.Repositories;
 using Microsoft.OpenApi.Models;
 using TodoApi.Core.UseCases;
 using TodoApi.Core.Interfaces;
+using DotNetEnv;
+
+Env.Load();
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +14,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 // Configure MySQL
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+// var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var connectionString = $"Server={Env.GetString("DB_SERVER")};" +
+                    $"Port={Env.GetString("DB_PORT")};" +
+                    $"Database={Env.GetString("DB_NAME")};" +
+                    $"Uid={Env.GetString("DB_USER")};" +
+                    $"Pwd={Env.GetString("DB_PASSWORD")}";
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
